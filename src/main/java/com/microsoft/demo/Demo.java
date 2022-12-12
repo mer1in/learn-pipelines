97f57b67efaacafb25af0812304b271c53578b60
Roman <roman@briefcam.com>
2022-12-12 21:41:05 +0200

    Revert "lets break the build"
    This reverts commit 2861eb95f8b3d42071a8c704c5a71f2d79812dac.

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -3,7 +3,6 @@ package com.microsoft.demo;
 public class Demo {
     public void DoSomething(boolean flag){
         if(flag){
-            nonexistentfunc();
             System.out.println("I am covered");
             return;
         }
