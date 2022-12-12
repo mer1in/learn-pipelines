2861eb95f8b3d42071a8c704c5a71f2d79812dac
Roman <roman@briefcam.com>
2022-12-12 21:36:27 +0200

    lets break the build

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -3,6 +3,7 @@ package com.microsoft.demo;
 public class Demo {
     public void DoSomething(boolean flag){
         if(flag){
+            nonexistentfunc();
             System.out.println("I am covered");
             return;
         }
